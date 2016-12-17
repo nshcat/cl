05b51b382cd6ef792d28a100ae55427cdb449ecc
nshcat <nshcat@users.noreply.github.com>
2016-12-17 02:13:28 +0100

    Modifed positional_argument to use the new multiple value base class.

--- include/cl/positional_argument.hxx
+++ include/cl/positional_argument.hxx
@@ -7,51 +7,29 @@
 #include <cstdlib>
 #include <algorithm>
 #include "tags.hxx"
-#include "value_base.hxx"
+#include "multi_base.hxx"
 #include "default_reader.hxx"
 
 namespace cl
 {
 	template<	typename T,
-				typename TReader = default_reader<T>,
-				typename TContainer = ::std::vector<T>
+				typename TReader = default_reader<T>
 	>
-	class positional_argument
-		: public internal::value_base<TContainer>
+	class positional_argument /* Argument type that greedily matches values. */
+		: public internal::multi_base<T, TReader, internal::multi_mode::positional>
 	{
-		using Tbase = internal::value_base<TContainer>;
-		using Tthis = positional_argument<T, TReader, TContainer>;
-		
-		// TODO static check if TContainer has push_back
-		// TODO maybe use something other than push_back? Maybe insert(end)?
+		using base_type = internal::multi_base<T, TReader, internal::multi_mode::positional>;
+		using this_type = positional_argument<T, TReader>;
 		
 		public:
 			template< typename... Ttags >
-			positional_argument(const Ttags&... p_tags)
-				: Tbase{}
+			positional_argument(Ttags&&... p_tags)
+				: base_type{}
 			{
-				this->template dispatch_all<Tthis>(p_tags...);
+				this->template dispatch_all<this_type>(::std::forward<Ttags>(p_tags)...);
 			}
 
 		public:
-			void read(std::list<std::string>& p_args, bool) override
-			{
-				// Consume all supplied strings and try to convert them to T
-				for(const auto& p_str: p_args)
-				{
-					this->m_Value.push_back(m_Reader.read(p_str));
-				}
-
-				// Clear input list since we consumed everything
-				p_args.clear();
-
-				this->m_Supplied = true;
-			}
-
-		public:
-			using Tbase::dispatch;
-			
-		protected:
-			TReader m_Reader;
+			using base_type::dispatch;	
 	};
 }
